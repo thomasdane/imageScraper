c4e7cc03bf179ec949a8d6173a99a6ac9dec86f8
Thomas Dane <tjdane@gmail.com>
2016-10-23 23:55:43 +0100

    let lambda know the function is complete with context.done

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+var AWS = require('aws-sdk');
+var request = require('request');
 
-	var AWS = require('aws-sdk');
-	var request = require('request');
+var scrapeImage = function(context) {
+	
 	AWS.config.loadFromPath('config.json');
-
 	var s3bucket = new AWS.S3({params: {Bucket: 'partywave-images'}});
 	var options = {
 		uri: 'http://i.imgur.com/0cIbmeM.png', //not to spam while testing
 		//uri: 'http://res3.seabreeze.com.au/images/forecast/2/syd1/grg.png', 
 		encoding: null
-	};
+	}
 
-	request(options, function(error, response, body){
+	request(options, function (error, response, body){
 		if(error || response.statusCode !==200){
 			console.log("failed to get image", error);
 		} else {
@@ -24,10 +25,15 @@
 				if (error) {
 					console.log("error saving image to S3", error);
 				} else {
-					console.log("success uploading image")
+					context.done(null, "success uploading image");					
 				}
 			});
 		}
-	});
+	})
+}
+
+exports.handler = function(event, context, callback) {
+	scrapeImage(context);
+};
 
 
